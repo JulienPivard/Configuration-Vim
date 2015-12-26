41c4110fba5629ccdcd9f242c2aa3ba239c67ba9
Asmodee <pivardjulien@gmail.com>
2015-12-26 18:08:41 +0100

    Ajout d'un cas possible dans code basique de Perl.

--- CodeBasique/codeBasique.pl
+++ CodeBasique/codeBasique.pl
@@ -2,6 +2,9 @@
 # Pour avoir un meilleur contrôle des règles du langage avant exécution.
 use strict;
 use warnings;
+# Pour utiliser say il faut une version minimum.
+#use v5.10;
+#say();
 
 {
     my $i = "\n§" . "(=^.^=)" x 4 . "§\n";
