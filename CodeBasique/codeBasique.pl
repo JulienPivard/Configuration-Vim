c1b4a4f96ef52cfc93a82b1492c0159ed7699f47
Asmodee <pivardjulien@gmail.com>
2016-01-21 23:46:48 +0100

    Problème d'encodage de caractères avec perl réglé.

--- CodeBasique/codeBasique.pl
+++ CodeBasique/codeBasique.pl
@@ -2,6 +2,7 @@
 # Pour avoir un meilleur contrôle des règles du langage avant exécution.
 use strict;
 use warnings;
+use utf8;
 # Pour utiliser say il faut une version minimum.
 #use v5.10;
 #say();
